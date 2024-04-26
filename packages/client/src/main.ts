13e1741f94342d1d1fddb63b20c685d6a35934f1
Vlad Faust <hey@vladfaust.com>
2024-04-26 17:16:00 +0700

    dx(client): add prettier

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -1,8 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import { invoke } from '@tauri-apps/api'
+import { invoke } from "@tauri-apps/api";
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./style.css";
 
-invoke('greet', { name: 'Tauri' }).then((response) => console.log(response))
+invoke("greet", { name: "Tauri" }).then((response) => console.log(response));
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
