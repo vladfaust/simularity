cfd5c9758f5f9ddde200e8b1c57268e958757381
Vlad Faust <hey@vladfaust.com>
2024-05-27 14:16:36 +0700

    feat(client): do not listen for ⌘+Q

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -3,11 +3,6 @@ import { useRoute, useRouter } from "vue-router";
 import { appWindow } from "@tauri-apps/api/window";
 import { onMounted, onUnmounted } from "vue";
 import { type UnlistenFn } from "@tauri-apps/api/event";
-import {
-  isRegistered,
-  register,
-  unregister,
-} from "@tauri-apps/api/globalShortcut";
 import { routeLocation } from "./lib/router";
 
 const route = useRoute();
@@ -30,25 +25,10 @@ onMounted(async () => {
       console.debug("onCloseRequested listener registered");
       unlisten = unlistenFn;
     });
-
-  isRegistered("Command+Q").then((registered) => {
-    if (!registered) {
-      register("Command+Q", async () => {
-        console.log("Command+Q pressed");
-        unregister("Command+Q");
-        router.push(routeLocation({ name: "Shutdown" }));
-        await cleanup();
-        await appWindow.close();
-      }).then(() => {
-        console.debug("Command+Q registered");
-      });
-    }
-  });
 });
 
 onUnmounted(() => {
   unlisten?.();
-  unregister("Command+Q");
 });
 </script>
 
