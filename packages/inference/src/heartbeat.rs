d1fffa2455d92ef800773c1de87797f856ee9367
Vlad Faust <hey@vladfaust.com>
2024-06-23 20:35:23 +0400

    fix(inference): re-register node if was offline

--- packages/inference/src/heartbeat.rs
+++ packages/inference/src/heartbeat.rs
@@ -1,5 +1,5 @@
 use log::{debug, info};
-use reqwest_middleware::ClientBuilder;
+use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
 use reqwest_retry::{policies::ExponentialBackoff, RetryTransientMiddleware};
 use std::collections::HashMap;
 
@@ -22,27 +22,7 @@ pub async fn run_heartbeat() {
         .with(RetryTransientMiddleware::new_with_policy(retry_policy))
         .build();
 
-    {
-        let gpt_model =
-            std::env::var("GPT_MODEL_NAME").expect("GPT_MODEL_NAME env variable is not set");
-        let node_base_url =
-            std::env::var("NODE_BASE_URL").expect("NODE_BASE_URL env variable is not set");
-
-        let mut body = HashMap::new();
-        body.insert("id", &node_id);
-        body.insert("gptModel", &gpt_model);
-        body.insert("baseUrl", &node_base_url);
-
-        client
-            .post(format!("{api_base_url}/inference-nodes"))
-            .header("Authorization", &authorization_header)
-            .json(&body)
-            .send()
-            .await
-            .unwrap();
-
-        info!("Registered node with the API server");
-    }
+    register_node(&client, &api_base_url, &node_id, &authorization_header).await;
 
     let heartbeat_interval =
         std::env::var("API_HEARTBEAT").expect("API_HEARTBEAT env variable is not set");
@@ -50,14 +30,49 @@ pub async fn run_heartbeat() {
     let heartbeat_url = format!("{api_base_url}/inference-nodes/{node_id}/heartbeat");
 
     loop {
-        client
+        let result = client
             .head(&heartbeat_url)
             .header("Authorization", &authorization_header)
             .send()
             .await
             .unwrap();
 
+        // If status is 404, the node has been deregistered.
+        // Re-register the node and continue sending heartbeats.
+        if result.status() == reqwest::StatusCode::NOT_FOUND {
+            info!("Node has been deregistered. Re-registering node with the API server");
+            register_node(&client, &api_base_url, &node_id, &authorization_header).await;
+            continue;
+        }
+
         debug!("Sent heartbeat to the API server");
         tokio::time::sleep(heartbeat_interval).await;
     }
+}
+
+async fn register_node(
+    client: &ClientWithMiddleware,
+    api_base_url: &str,
+    node_id: &str,
+    authorization_header: &str,
+) {
+    let gpt_model =
+        std::env::var("GPT_MODEL_NAME").expect("GPT_MODEL_NAME env variable is not set");
+    let node_base_url =
+        std::env::var("NODE_BASE_URL").expect("NODE_BASE_URL env variable is not set");
+
+    let mut body = HashMap::new();
+    body.insert("id", node_id);
+    body.insert("gptModel", &gpt_model);
+    body.insert("baseUrl", &node_base_url);
+
+    client
+        .post(format!("{api_base_url}/inference-nodes"))
+        .header("Authorization", authorization_header)
+        .json(&body)
+        .send()
+        .await
+        .unwrap();
+
+    info!("Registered node with the API server");
 }
