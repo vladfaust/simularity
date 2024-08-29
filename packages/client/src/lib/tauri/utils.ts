bb45e2041b4a9db0b69adc470af84baec56ca58d
Vlad Faust <hey@vladfaust.com>
2024-08-29 16:34:07 +0400

    fixup! feat(client): manage local models

--- packages/client/src/lib/tauri/utils.ts
+++ packages/client/src/lib/tauri/utils.ts
@@ -2,6 +2,8 @@ import { invoke } from "@tauri-apps/api";
 
 /**
  * Open a file manager at the specified path.
+ * Unlike [`shell.open`](https://tauri.app/v1/api/js/shell/#open),
+ * this command allows to select a file.
  */
 export async function fileManagerOpen(path: string): Promise<void> {
   await invoke("file_manager_open", { path });
