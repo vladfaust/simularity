859a7eff4023d6412f2e1ad73f5b196a279d4de7
Vlad Faust <hey@vladfaust.com>
2024-09-20 23:49:05 +0400

    temp! another thing

--- packages/core-rs/build.rs
+++ packages/core-rs/build.rs
@@ -1,10 +1,31 @@
+use std::env;
+use std::path::Path;
+
 fn main() {
+    let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
     println!("cargo:rerun-if-changed=../core-cpp/src");
     println!("cargo:rustc-link-lib=c++");
 
-    println!("cargo::rustc-link-search=../core-cpp/build");
-    println!("cargo::rustc-link-search=../core-cpp/build/vendor/llama.cpp/src");
-    println!("cargo::rustc-link-search=../core-cpp/build/vendor/ggml/src");
+    // println!("cargo::rustc-link-search=../core-cpp/build");
+    println!(
+        "cargo::rustc-link-search={}",
+        Path::new(&dir).join("../core-cpp/build").display()
+    );
+    // println!("cargo::rustc-link-search=../core-cpp/build/vendor/llama.cpp/src");
+    println!(
+        "cargo::rustc-link-search={}",
+        Path::new(&dir)
+            .join("../core-cpp/build/vendor/llama.cpp/src")
+            .display()
+    );
+    // println!("cargo::rustc-link-search=../core-cpp/build/vendor/ggml/src");
+    println!(
+        "cargo::rustc-link-search={}",
+        Path::new(&dir)
+            .join("../core-cpp/build/vendor/ggml/src")
+            .display()
+    );
 
     #[cfg(target_os = "macos")]
     {
