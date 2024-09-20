ee3acace2a55dcf306979d7c95d0cdaedaef7782
Vlad Faust <hey@vladfaust.com>
2024-09-20 23:06:31 +0400

    temp! thsi thing

--- packages/tauri/scripts/config/windows.js
+++ packages/tauri/scripts/config/windows.js
@@ -3,6 +3,6 @@ import fs from "fs";
 const tauriConf = JSON.parse(fs.readFileSync("tauri.conf.json", "utf-8"));
 
 tauriConf.build.distDir = "..\\client\\dist";
-tauriConf.tauri.bundle.resources = ["..\\client\\resources"];
+tauriConf.tauri.bundle.resources = ["..\\client\\resources2"];
 
 fs.writeFileSync("tauri.conf.local.json", JSON.stringify(tauriConf));
