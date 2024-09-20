eb81009dfb290b6fff2f0a71016f5d22a07fb2ce
Vlad Faust <hey@vladfaust.com>
2024-09-20 15:29:31 +0400

    fixup! chores(client): build on windows
    Add config:windows script

---
+++ packages/tauri/scripts/config/windows.js
@@ -0,0 +1,8 @@
+import fs from "fs";
+
+const tauriConf = JSON.parse(fs.readFileSync("tauri.conf.json", "utf-8"));
+
+tauriConf.build.distDir = "..\\client\\dist";
+tauriConf.tauri.bundle.resources = ["..\\client\\resources"];
+
+fs.writeFileSync("tauri.conf.local.json", JSON.stringify(tauriConf));
